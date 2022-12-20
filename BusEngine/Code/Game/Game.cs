63b1cb72291571b4f94e1e6e77252120f3d0bd08
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2022-12-20 14:23:52 +0200

    Add files via upload

--- BusEngine/Code/Game/Game.cs
+++ BusEngine/Code/Game/Game.cs
@@ -3,7 +3,7 @@
 
 /* C# 6.0+              https://learn.microsoft.com/ru-ru/dotnet/csharp/whats-new/csharp-version-history */
 /* NET.Framework 4.6.1+ https://learn.microsoft.com/ru-ru/dotnet/framework/migration-guide/versions-and-dependencies */
-/* MSBuild 12.0+        https://en.wikipedia.org/wiki/MSBuild#Versions */
+/* MSBuild 14.0+        https://en.wikipedia.org/wiki/MSBuild#Versions */
 /* MSBuild 15.0+        https://learn.microsoft.com/en-us/xamarin/android/app-fundamentals/android-api-levels?tabs=windows#android-versions */
 /* Mono                 https://learn.microsoft.com/ru-ru/xamarin/android/deploy-test/building-apps/abi-specific-apks */
 /* ссылки по Android 
@@ -45,7 +45,6 @@ BusEngine.UI
 	}
 
 	internal class Start {
-		//public const bool BUSENGINE_WINFORM = true;
 		/** функция запуска приложения */
 		//[System.STAThread] // если однопоточное приложение
 		private static void Main(string[] args) {
@@ -65,6 +64,7 @@ BusEngine.UI
 			}
 			*/
 
+			// генерируем BusEngine API
 			BusEngine.Engine.generateStatLink();
 
 
@@ -146,10 +146,19 @@ BusEngine.UI
 			/* BusEngine.Plugin _plugin = new BusEngine.Game.Default();
 			_plugin.Initialize(); */
 
-			//BusEngine.Log.Debug();
 
+
+
+
+
+
+
+
+
+			// создаём форму System.Windows.Forms
 			Form _form = new Form();
-			//#if BUSENGINE
+
+			// покдлючаем  BusEngine API
 			BusEngine.UI.Canvas.WinForm = _form;
 			BusEngine.UI.Canvas.Initialize();
 			//if (typeof(BusEngine.UI.Canvas).GetField("WinForm") != null) {
@@ -165,50 +174,52 @@ BusEngine.UI
 			//BusEngine.Video.Play("Videos/BusEngine.mp4");
 
 			// запускаем браузер
-			//CefSharp.BrowserSubprocess.SelfHost.Main(args);
 			BusEngine.Browser.Start("index.html");
+
+			// запускаем приложение System.Windows.Forms
 			System.Windows.Forms.Application.Run(_form);
-			//#else
-				
-			//#endif
 		}
 		/** функция запуска приложения */
 	}
 
+	// https://learn.microsoft.com/ru-ru/dotnet/api/system.windows.forms.form?view=netframework-4.8
 	internal class Form : System.Windows.Forms.Form {
 		/** функция запуска окна приложения */
-		//#region Windows Form Designer generated code
 		public Form() {
-			// https://learn.microsoft.com/ru-ru/dotnet/api/system.windows.forms.form?view=netframework-4.8
-			//System.Windows.Forms.Form _form = new System.Windows.Forms.Form();
 			// название окна
-			//System.Windows.Forms.Form _canvas = System.Windows.Forms.Form();
 			this.Text = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + " BusEngine v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
 			// устанавливаем нашу иконку, есди она есть по пути exe, в противном случае устанавливаем системную
 			if (System.IO.File.Exists(BusEngine.Engine.DataDirectory + "Icons/BusEngine.ico")) {
 				this.Icon = new System.Drawing.Icon(System.IO.Path.Combine(BusEngine.Engine.DataDirectory, "Icons/BusEngine.ico"), 128, 128);
 			} else {
 				this.Icon = new System.Drawing.Icon(System.Drawing.SystemIcons.Exclamation, 128, 128);
 			}
+
 			// устанавливаем размеры окна
 			this.Width = 1024;
 			this.Height = 768;
+
 			// центрируем окно
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+
 			// открываем окно на весь экран
 			//this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+
 			// убираем линии, чтобы окно было полностью на весь экран
 			//this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+
 			// устанавливаем чёрный цвет фона окна
 			this.BackColor = System.Drawing.Color.Black;
-			// устанавливаем события нажатий клавиш
+
+			// устанавливаем событие нажатий клавиш
 			this.KeyPreview = true;
 			//this.KeyDown += OnKeyDown;
+
 			// устанавливаем событие закрытия окна
 			//this.FormClosed += OnClosed;
 			//this.Disposed += new System.EventHandler(OnDisposed);
 			//ClientSize = this.ClientSize;
-			System.Console.WriteLine(this.ClientSize);
 
 			// показываем форму\включаем\запускаем\стартуем показ окна
 			//this.ShowDialog();
@@ -219,8 +230,6 @@ BusEngine.UI
 				//_winform = winform;
 			//}
 		//}
-
-		//#endregion
 
 		private const int WM_ACTIVATEAPP = 0x001C;
 		private bool appActive = true;
