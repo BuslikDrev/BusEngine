60a6c0e7a9e56c279d471caa7f126e2e2c046a2e
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2022-12-21 19:37:22 +0200

    Add files via upload

--- BusEngine/Code/Game/Game.cs
+++ BusEngine/Code/Game/Game.cs
@@ -4,29 +4,9 @@
 /* C# 6.0+              https://learn.microsoft.com/ru-ru/dotnet/csharp/whats-new/csharp-version-history */
 /* NET.Framework 4.6.1+ https://learn.microsoft.com/ru-ru/dotnet/framework/migration-guide/versions-and-dependencies */
 /* MSBuild 14.0+        https://en.wikipedia.org/wiki/MSBuild#Versions */
-/* MSBuild 15.0+        https://learn.microsoft.com/en-us/xamarin/android/app-fundamentals/android-api-levels?tabs=windows#android-versions */
-/* Mono                 https://learn.microsoft.com/ru-ru/xamarin/android/deploy-test/building-apps/abi-specific-apks */
-/* ссылки по Android 
-https://learn.microsoft.com/ru-ru/xamarin/android/app-fundamentals/permissions?tabs=windows
-*/
 
 /** дорожная карта
-- проставить нормально модификаторы доступа https://metanit.com/sharp/tutorial/3.2.php
-- максимально весь функционал сделать независимыми плагинами и установить проверки
- на наличие плагинов перед их использованием
-- апи перенести в проект библиотеки, а текущий, только для запуска и остановки программы,
- с возможностью отказа от использования API BusEngine
-- создать: генерацию сцены (карты), камеру, консольные команды, консоль, настройка проекта
-- написать лаунчер с возможностью: безопасной регистрации, безопасной авторизации,
- скачать движок, восстановить файлы движка, удалить движок, создать проект, собрать проект,
- сгенерировать csproj, удалить проект, изменить язык лаунчера, настроить путь компилятора
- под каждый проект, настроить платформу для проекта с помощью чекбокса, добавить универсальное
- поле указания конфигурации csproj, вывести блок информации из сайта с ограничением в 4-6 шт
- (новости index.php?route=api/busengine/information&order=DESC&limit=4, последние товары кроме
- плагинов index.php?route=api/busengine/product&order=DESC&limit=4&type_exception=["plugin"],
- последние плагины index.php?route=api/busengine/product&order=DESC&limit=4&type=["plugin"])
-- написать редактор игры и добавить управление тем, чем располагает текущий АПИ
-- написать сборку игры для windows 7+ и Android 5+
+- написать запуск игры BusEngine
 */
 
 #define BUSENGINE_WINFORM
@@ -67,7 +47,7 @@ BusEngine.UI
 			// генерируем BusEngine API
 			BusEngine.Engine.generateStatLink();
 
-
+			BusEngine.Engine.platform = "BUSENGINE_WINFORM";
 
 
 
@@ -151,9 +131,9 @@ BusEngine.UI
 
 
 
-
-
-
+			BusEngine.Log.Info("============== ajax запустили");
+			BusEngine.Ajax.Test("https://buslikdrev.by/");
+			BusEngine.Log.Info("============== ajax запустили");
 
 			// создаём форму System.Windows.Forms
 			Form _form = new Form();
