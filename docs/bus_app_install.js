2b557d3649c236af9948d15bbb026b893bcf9ef8
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2022-12-30 23:41:32 +0200

    Add files via upload

--- docs/bus_app_install.js
+++ docs/bus_app_install.js
@@ -12,7 +12,7 @@ var busApp = {
 			},
 			"cache_status":true,
 			"cache_resources":[],
-			"cache_resources_exception":"",
+			"cache_resources_exception":"https://translate.google.com/,https://www.gstatic.com/",
 			"cache_max_ages":604800,
 			"cache_token":1,
 			"push_status":false,
