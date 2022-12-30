c994000477716620287794951a7a2bed674e8fad
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2022-12-30 23:37:51 +0200

    Add files via upload

--- docs/Scripts/BusEngine/bus_app.js
+++ docs/Scripts/BusEngine/bus_app.js
@@ -26,7 +26,7 @@ var busApp = {
 		cache_resources:[],
 		cache_resources_exception:"",
 		cache_max_ages:604800,
-		cache_token:1,
+		cache_token:2,
 		notification_status:false,
 		notification_interval:3600,
 		notification_error:true,
