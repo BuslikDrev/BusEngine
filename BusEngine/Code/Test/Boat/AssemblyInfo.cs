14bda9c972ccfaf02f8b499dc6885bc16ac6b30a
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2023-01-06 13:34:12 +0200

    Add files via upload

---
+++ BusEngine/Code/Test/Boat/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿/* Аўтар: "БуслікДрэў" ( https://buslikdrev.by/ ) */
+/* © 2016-2023; BuslikDrev - Усе правы захаваны. */
+
+/* C# 6.0+              https://learn.microsoft.com/ru-ru/dotnet/csharp/whats-new/csharp-version-history */
+/* NET.Framework 4.6.2+ https://learn.microsoft.com/ru-ru/dotnet/framework/migration-guide/versions-and-dependencies */
+/* MSBuild 14.0+        https://en.wikipedia.org/wiki/MSBuild#Versions */
+
+[assembly: System.Reflection.AssemblyTitle("BusEngine Game")]
+[assembly: System.Reflection.AssemblyDescription("The main C# interface for the BusEngine.")]
+[assembly: System.Reflection.AssemblyConfiguration("")]
+[assembly: System.Reflection.AssemblyCompany("BuslikDrev")]
+[assembly: System.Reflection.AssemblyProduct("BusEngine")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright 2016-2023 BuslikDrev. All rights reserved.")]
+[assembly: System.Reflection.AssemblyTrademark("")]
+[assembly: System.Reflection.AssemblyCulture("")]
+[assembly: System.Reflection.AssemblyVersion("0.3.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("0.3.0.0")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Runtime.InteropServices.Guid("0968cb8e-b0e3-46c7-96a9-2efb99223941")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("BusEngine")]
