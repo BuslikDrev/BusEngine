faf58e0aa194842635f86142000e0d86bc926726
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2023-04-18 12:53:01 +0300

    Add files via upload

--- BusEngine/Templates/cs/Launcher/Code/Plugin/Program.cs
+++ BusEngine/Templates/cs/Launcher/Code/Plugin/Program.cs
@@ -8,6 +8,7 @@ namespace BusEngine.Game {
 	/** API BusEngine.Plugin */
 	public class MyPlugin : BusEngine.Plugin {
 		//private static System.Windows.Forms.Form SplashScreen;
+		private System.ComponentModel.IContainer notifyIconComponents;
 
 		// при запуске BusEngine до создания формы
 		public override void Initialize() {
@@ -33,8 +34,8 @@ namespace BusEngine.Game {
 			SplashScreen.Show(); */
 
 			// устанавливаем настройки
-			BusEngine.Engine.SettingEngine["console_commands"]["r_Width"] = "1280";
-			BusEngine.Engine.SettingEngine["console_commands"]["r_Height"] = "720";
+			//BusEngine.Engine.SettingEngine["console_commands"]["r_Width"] = "1280";
+			//BusEngine.Engine.SettingEngine["console_commands"]["r_Height"] = "720";
 
 			//System.Threading.Tasks.Task.Delay(2000).Wait();
 		}
@@ -44,18 +45,52 @@ namespace BusEngine.Game {
 			// убираем стартовую обложку
 			/* SplashScreen.Close();
 			SplashScreen.Dispose(); */
-			
+
 			//BusEngine.UI.Canvas.WinForm.TransparencyKey = System.Drawing.Color.Black;
 			//BusEngine.UI.Canvas.WinForm.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			//BusEngine.UI.Canvas.WinForm.Width = 800;
-			//BusEngine.UI.Canvas.WinForm.Height = 480;
 			// деламем окно не поверх других окон
 			BusEngine.UI.Canvas.WinForm.TopMost = false;
+
+			// добавляем иконку в системном трее
+			/* notifyIconComponents = new System.ComponentModel.Container();
+			System.Windows.Forms.ContextMenu contextMenu = new System.Windows.Forms.ContextMenu();
+			System.Windows.Forms.MenuItem menuItem1 = new System.Windows.Forms.MenuItem();
+			menuItem1.Index = 0;
+			menuItem1.Text = BusEngine.Localization.GetLanguageStatic("button_exit");
+			menuItem1.Click += new System.EventHandler((o, e) => {
+				BusEngine.Engine.Shutdown();
+			});
+
+			System.Windows.Forms.MenuItem menuItem2 = new System.Windows.Forms.MenuItem();
+			menuItem2.Index = 1;
+			menuItem2.Text = BusEngine.Localization.GetLanguageStatic("button_donate");
+			menuItem2.Click += new System.EventHandler((o, e) => {
+				System.Diagnostics.Process.Start("Url");
+			});
+
+			contextMenu.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {menuItem1, menuItem2});
+
+			System.Windows.Forms.NotifyIcon notifyIcon = new System.Windows.Forms.NotifyIcon(notifyIconComponents);
+			notifyIcon.Icon = BusEngine.UI.Canvas.WinForm.Icon;
+			notifyIcon.ContextMenu = contextMenu;
+			notifyIcon.Text = BusEngine.UI.Canvas.WinForm.Text;
+			notifyIcon.Visible = true;
+			BusEngine.UI.Canvas.WinForm.FormClosing += (o, e) => {
+				if (notifyIconComponents != null) {
+					notifyIconComponents.Dispose();
+				}
+			}; */
 
 			// запускаем браузер
 			Browser("index.html");
 		}
-		
+
+		private void OnDisposed(object Sender, System.EventArgs e) {
+			if (notifyIconComponents != null) {
+				notifyIconComponents.Dispose();
+			}
+		}
+
 		private static void Paint3(object sender, System.Windows.Forms.PaintEventArgs e) {
 			// труба (прямоугольник)
 			e.Graphics.FillRectangle(new System.Drawing.SolidBrush(System.Drawing.Color.Black), 480, 125, 75, 75);
@@ -63,27 +98,88 @@ namespace BusEngine.Game {
 
 		// запускаем браузер WinForm только в одном потоке =(
 		private void Browser(string url) {
+			int w = BusEngine.UI.Canvas.WinForm.Width,
+			h = BusEngine.UI.Canvas.WinForm.Height,
+			x = BusEngine.UI.Canvas.WinForm.DesktopLocation.X,
+			y = BusEngine.UI.Canvas.WinForm.DesktopLocation.Y;
+
 			BusEngine.Browser.Initialize(url);                                                   
 			BusEngine.Browser.OnPostMessageStatic += (string message) => {
-				if (message == "Collapse") {
+				string t = message.ToLower();
+
+				if (t == "collapse") {
 					BusEngine.UI.Canvas.WinForm.WindowState = System.Windows.Forms.FormWindowState.Minimized;
-				} else if (message == "Expand") {
+				} else if (t == "expand") {
 					if (BusEngine.UI.Canvas.WinForm.WindowState == System.Windows.Forms.FormWindowState.Maximized) {
 						BusEngine.UI.Canvas.WinForm.WindowState = System.Windows.Forms.FormWindowState.Normal;
 					} else {
 						BusEngine.UI.Canvas.WinForm.WindowState = System.Windows.Forms.FormWindowState.Maximized;
 					}
-				} else if (message == "Exit") {
+				} else if (t == "exit") {
 					BusEngine.Engine.Shutdown();
-				} else if (message == "Debug") {
+				} else if (t == "debug") {
 					BusEngine.Log.Info("JavaScript: Привет CSharp!");
 					BusEngine.Log.Info("На команду: " + message);
 					BusEngine.Browser.ExecuteJSStatic("document.dispatchEvent(new CustomEvent('BusEngineMessage', {bubbles: true, detail: {hi: 'CSharp: Прювэт JavaScript!', data: 'Получил твою команду! Вось яна: " + message + "'}}));");
 				} else {
-					if (message.Substring(0, 8) == "console|") {
+					t = t.Substring(0, 8);
+					if (t == "console|") {
 						System.Console.ForegroundColor = System.ConsoleColor.Cyan;
 						BusEngine.Log.Info("Console: {0}", message.Substring(8));
 						System.Console.ResetColor();
+					} else if (t == "_resize|") {
+						System.Console.ForegroundColor = System.ConsoleColor.Green;
+
+						string[] xy = message.Substring(8).Split(' ');
+						if (xy.Length > 0) {
+							int nx = System.Convert.ToInt32(xy[0]);
+							int ny = 0;
+
+							if (xy.Length > 1) {
+								ny = System.Convert.ToInt32(xy[1]);
+							}
+
+							//BusEngine.UI.Canvas.WinForm.DesktopLocation = new System.Drawing.Point(BusEngine.UI.Canvas.WinForm.DesktopLocation.X + nx, BusEngine.UI.Canvas.WinForm.DesktopLocation.Y + ny);
+							BusEngine.UI.Canvas.WinForm.Width += nx;
+							BusEngine.UI.Canvas.WinForm.Height += ny;
+							
+							
+							if (nx > w) {
+								//BusEngine.UI.Canvas.WinForm.Width = nx;
+								//BusEngine.UI.Canvas.WinForm.Height = System.Convert.ToInt32(h/w*nx);
+							}
+
+							// изменяем влево
+							if (nx < 0 || nx < 0) {
+
+								//BusEngine.UI.Canvas.WinForm.Width = w + (nx * -1);
+
+								//BusEngine.UI.Canvas.WinForm.Height = System.Convert.ToInt32(h/w*(w + (nx * -1)));
+								//BusEngine.UI.Canvas.WinForm.DesktopLocation.X += nx;
+								//BusEngine.UI.Canvas.WinForm.DesktopLocation = new System.Drawing.Point(x+nx, y);
+
+							} else {
+								
+							}
+
+							//BusEngine.Log.Info("Point-----: {0}", BusEngine.UI.Canvas.WinForm.DesktopLocation);
+							//BusEngine.Log.Info("resizeX-----: {0}", BusEngine.UI.Canvas.WinForm.Height);
+							//BusEngine.Log.Info("resizeX: {0}", nx);
+							System.Console.ResetColor();
+						}
+					} else if (t == "__point|") {
+						string[] xy = message.Substring(8).Split(' ');
+
+						if (xy.Length > 0) {
+							int nx = System.Convert.ToInt32(xy[0]);
+							int ny = 0;
+
+							if (xy.Length > 1) {
+								ny = System.Convert.ToInt32(xy[1]);
+							}
+
+							BusEngine.UI.Canvas.WinForm.DesktopLocation = new System.Drawing.Point(BusEngine.UI.Canvas.WinForm.DesktopLocation.X + nx, BusEngine.UI.Canvas.WinForm.DesktopLocation.Y + ny);
+						}
 					}
 				}
 			};
