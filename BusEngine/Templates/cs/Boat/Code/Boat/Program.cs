329f405cd9274f89c181ae5b301679421a5a6991
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2023-11-29 01:33:42 +0300

    Add files via upload

--- BusEngine/Templates/cs/Boat/Code/Boat/Program.cs
+++ BusEngine/Templates/cs/Boat/Code/Boat/Program.cs
@@ -1,13 +1,6 @@
 /* Аўтар: "БуслікДрэў" ( https://buslikdrev.by/ ) */
 /* © 2016-2024; BuslikDrev - Усе правы захаваны. */
 
-//https://youtu.be/K8eW4cPIlx0?t=2133
-
-//#define BROWSER_LOG
-#define LOCALIZATION_LOG
-
-using System;
-
 /** API BusEngine.Game - пользовательский код */
 namespace BusEngine.Game {
 	/** API BusEngine.Plugin */
@@ -29,8 +22,13 @@ namespace BusEngine.Game {
 		private static System.Drawing.SolidBrush myBlue;
 		private static System.Drawing.Pen myWind;
 
-		// при запуске BusEngine до создания формы
-		public /* async */ override void Initialize() {
+		// при запуске BusEngine после создания формы Canvas
+		public /* async */ override void InitializeСanvas() {
+			//BusEngine.UI.Canvas.WinForm.TransparencyKey = System.Drawing.Color.Black;
+			//BusEngine.UI.Canvas.WinForm.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			// деламем окно не поверх других окон
+			BusEngine.UI.Canvas.WinForm.TopMost = false;
+
 			// создаем объекты-кисти для закрашивания фигур
 			myTrub = new System.Drawing.SolidBrush(System.Drawing.Color.DeepPink);
 			myCorp = new System.Drawing.SolidBrush(System.Drawing.Color.DarkMagenta);
@@ -41,19 +39,10 @@ namespace BusEngine.Game {
 			myBlue = new System.Drawing.SolidBrush(System.Drawing.Color.Blue);
 			// выбираем перо myPen желтого цвета толщиной в 1 пиксель:
 			myWind = new System.Drawing.Pen(System.Drawing.Color.Yellow, 1);
-		}
-
-		// при запуске BusEngine после создания формы Canvas
-		public /* async */ override void InitializeСanvas() {
-			//BusEngine.UI.Canvas.WinForm.TransparencyKey = System.Drawing.Color.Black;
-			//BusEngine.UI.Canvas.WinForm.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			// деламем окно не поверх других окон
-			BusEngine.UI.Canvas.WinForm.TopMost = false;
 
 			// тест графики
 			// https://rsdn.org/article/gdi/gdiplus2mag.xml
-			BusEngine.UI.Canvas.WinForm.Paint += new System.Windows.Forms.PaintEventHandler(Paint);
-			//BusEngine.UI.Canvas.WinForm.Paint += new System.Windows.Forms.PaintEventHandler(Paint1);
+			BusEngine.UI.Canvas.WinForm.Paint += new System.Windows.Forms.PaintEventHandler(Paint1);
 			BusEngine.UI.Canvas.WinForm.Paint += new System.Windows.Forms.PaintEventHandler(Paint2);
 			BusEngine.UI.Canvas.WinForm.Paint += new System.Windows.Forms.PaintEventHandler(Paint3);
 			BusEngine.UI.Canvas.WinForm.Paint += new System.Windows.Forms.PaintEventHandler(Paint4);
@@ -61,33 +50,62 @@ namespace BusEngine.Game {
 			BusEngine.UI.Canvas.WinForm.Paint += new System.Windows.Forms.PaintEventHandler(Paint6);
 			BusEngine.UI.Canvas.WinForm.Paint += new System.Windows.Forms.PaintEventHandler(Paint7);
 			BusEngine.UI.Canvas.WinForm.Paint += new System.Windows.Forms.PaintEventHandler(Paint8);
+
 			// подключаем событие мыши
 			BusEngine.UI.Canvas.WinForm.MouseMove += new System.Windows.Forms.MouseEventHandler(MouseMove);
 
 			/* // зависимость от времени
 			System.Timers.Timer aTimer = new System.Timers.Timer(1000F/FPSSetting);
-			// Hook up the Elapsed event for the timer.
-			// подключаем событие времени
 			aTimer.Elapsed += OnTimedEvent;
 			aTimer.AutoReset = true;
 			aTimer.Enabled = true; */
 
 			// FPS
 			System.Timers.Timer fpsTimer = new System.Timers.Timer(1000);
-			// Hook up the Elapsed event for the timer.
-			// подключаем событие FPS
 			fpsTimer.Elapsed += OnFPS;
 			fpsTimer.AutoReset = true;
 			fpsTimer.Enabled = true;
 		}
 
-		// событие мыши
-		private static void MouseMove(object sender, System.Windows.Forms.MouseEventArgs e) {
-			//BusEngine.Log.Clear();
+		// вызывается при отрисовки каждого кадра
+		public override void OnGameUpdate() {
+			FPS++;
+
+			if (Count < 1 || Count < 300 && Nap == true) {
+				Nap = true;
+			} else {
+				Nap = false;
+			}
+
+			if (Nap == true) {
+				Count++;
+			} else {
+				Count--;
+			}
+
+			Count2++;
+
+			BusEngine.Log.Clear();
 			BusEngine.Log.Info("FPS ============== FPS Setting " + FPSSetting);
 			BusEngine.Log.Info("FPS ============== FPS " + FPSInfo);
-			//BusEngine.UI.Canvas.WinForm.Refresh();
-			//BusEngine.UI.Canvas.WinForm.Invalidate();
+			BusEngine.Log.Info("Paint ============== Paint " + Count + " " + Count2);
+		}
+
+		// перед закрытием BusEngine
+		public override void Shutdown() {
+			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint1);
+			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint2);
+			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint3);
+			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint4);
+			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint5);
+			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint6);
+			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint7);
+			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint8);
+		}
+
+		// событие мыши
+		private static void MouseMove(object sender, System.Windows.Forms.MouseEventArgs e) {
+			BusEngine.UI.Canvas.WinForm.Invalidate();
 		}
 
 		// событие времени
@@ -112,28 +130,6 @@ namespace BusEngine.Game {
 			FPS = 0;
 		}
 
-		// событие отрисовки - главный подсчёт кадров
-		private static void Paint(object sender, System.Windows.Forms.PaintEventArgs e) {
-			FPS++;
-			//e.Graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighSpeed;
-
-			if (Count < 10 || Count < 300 && Nap == true) {
-				Nap = true;
-			} else {
-				Nap = false;
-			}
-
-			if (Nap == true) {
-				Count++;
-			} else {
-				Count--;
-			}
-
-			//if (Count/3 == System.Convert.ToInt32(Count/3)) {
-				Count2++;
-			//}
-		}
-
 		// событие отрисовки - модель 1
 		private static void Paint1(object sender, System.Windows.Forms.PaintEventArgs e) {
 			// фон
@@ -150,7 +146,6 @@ namespace BusEngine.Game {
 		private static void Paint3(object sender, System.Windows.Forms.PaintEventArgs e) {
 			// труба (прямоугольник)
 			e.Graphics.FillRectangle(myTrub, 480 + Count, 125, 75, 75);
-			//e.Graphics.FillRectangle(new System.Drawing.SolidBrush(System.Drawing.Color.Black), 480, 125, 75, 75);
 		}
 
 		// событие отрисовки - модель 4
@@ -174,19 +169,16 @@ namespace BusEngine.Game {
 		// событие отрисовки - модель 6
 		private static void Paint6(object sender, System.Windows.Forms.PaintEventArgs e) {
 			// корпус (трапеция)
-			e.Graphics.FillPolygon(
-				myCorp, 
-				new System.Drawing.Point[] {
-					new System.Drawing.Point(100 + Count, 300),
-					new System.Drawing.Point(700 + Count, 300),
-					new System.Drawing.Point(700 + Count, 300),
-					new System.Drawing.Point(600 + Count, 400),
-					new System.Drawing.Point(600 + Count, 400),
-					new System.Drawing.Point(200 + Count, 400),
-					new System.Drawing.Point(200 + Count, 400),
-					new System.Drawing.Point(100 + Count, 300)
-				}
-			);
+			e.Graphics.FillPolygon(myCorp, new System.Drawing.Point[] {
+				new System.Drawing.Point(100 + Count, 300),
+				new System.Drawing.Point(700 + Count, 300),
+				new System.Drawing.Point(700 + Count, 300),
+				new System.Drawing.Point(600 + Count, 400),
+				new System.Drawing.Point(600 + Count, 400),
+				new System.Drawing.Point(200 + Count, 400),
+				new System.Drawing.Point(200 + Count, 400),
+				new System.Drawing.Point(100 + Count, 300)
+			});
 		}
 
 		// событие отрисовки - модель 7
@@ -205,7 +197,7 @@ namespace BusEngine.Game {
 			//e.Graphics.TranslateTransform(0, 0);
 
 			// Save translated graphics state.
-			System.Drawing.Drawing2D.GraphicsState transState = e.Graphics.Save();
+			//System.Drawing.Drawing2D.GraphicsState transState = e.Graphics.Save();
 
 			// Reset transformation matrix to identity and fill rectangle.
 			e.Graphics.FillRectangle(myRed, 0, 0, 100, 100);
@@ -218,16 +210,7 @@ namespace BusEngine.Game {
 			//}
 
 			// Restore graphics state to translated state and fill second
-			e.Graphics.Restore(transState);
-			//FPS++;
-/* 			BusEngine.Log.Clear();
-			BusEngine.Log.Info("The Elapsed event was raised at {0}", e.Graphics.CompositingQuality);
-			//BusEngine.Log.Info("The Elapsed event was raised at {0:HH:mm:ss.fff}", e.SignalTime);
-			BusEngine.Log.Info(System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width);
-			BusEngine.Log.Info("FPS ============== FPS Setting " + FPSSetting);
-			BusEngine.Log.Info("FPS ============== FPS " + FPSInfo);
-			BusEngine.Log.Info("Paint ============== Paint " + Count + " " + Count2);
-			BusEngine.UI.Canvas.WinForm.Invalidate(); */
+			//e.Graphics.Restore(transState);
 		}
 
 		private static void ScaleTransformFloat(System.Windows.Forms.PaintEventArgs e) {
@@ -239,32 +222,7 @@ namespace BusEngine.Game {
 
 			// Draw scaled, rotated rectangle to screen.
 			e.Graphics.DrawRectangle(new System.Drawing.Pen(System.Drawing.Color.Blue, 3), 50, 0, 100, 40);
-		}
-
-		// вызывается при отрисовки каждого кадра
-		public override void OnGameUpdate() {
-			BusEngine.Log.Clear();
-			//BusEngine.Log.Info("The Elapsed event was raised at {0}", e.Graphics.CompositingQuality);
-			//BusEngine.Log.Info("The Elapsed event was raised at {0:HH:mm:ss.fff}", e.SignalTime);
-			BusEngine.Log.Info(System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width);
-			BusEngine.Log.Info("FPS ============== FPS Setting " + FPSSetting);
-			BusEngine.Log.Info("FPS ============== FPS " + FPSInfo);
-			BusEngine.Log.Info("Paint ============== Paint " + Count + " " + Count2);
-			BusEngine.UI.Canvas.WinForm.Invalidate();
-		}
-
-		// перед закрытием BusEngine
-		public override void Shutdown() {
-			//BusEngine.UI.Canvas.ShutdownStatic();
-			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint);
-			//BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint1);
-			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint2);
-			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint3);
-			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint4);
-			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint5);
-			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint6);
-			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint7);
-			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint8);
+			//BusEngine.UI.Canvas.WinForm.Update();
 		}
 	}
 	/** API BusEngine.Plugin */
