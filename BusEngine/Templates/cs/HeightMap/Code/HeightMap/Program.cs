c56910ef1fa14833e39c0cd34c067d0ea3b93738
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2023-11-29 12:06:26 +0300

    Add files via upload

--- BusEngine/Templates/cs/HeightMap/Code/HeightMap/Program.cs
+++ BusEngine/Templates/cs/HeightMap/Code/HeightMap/Program.cs
@@ -58,62 +58,52 @@ namespace BusEngine.Game {
 			fpsTimer.Enabled = true;
 
 			//BusEngine.Engine.GameStart();
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			//BusEngine.Log.ConsoleShow();
-            /* BusEngine.UI.Canvas.WinForm.SuspendLayout();
-            BusEngine.UI.Canvas.WinForm.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            BusEngine.UI.Canvas.WinForm.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            BusEngine.UI.Canvas.WinForm.ClientSize = new System.Drawing.Size(537, 403);
-            BusEngine.UI.Canvas.WinForm.Name = "Form1";
-            BusEngine.UI.Canvas.WinForm.Text = "Form1";
-            BusEngine.UI.Canvas.WinForm.ResumeLayout(false);
-
-            BusEngine.UI.Canvas.WinForm.SetStyle(System.Windows.Forms.ControlStyles.AllPaintingInWmPaint | System.Windows.Forms.ControlStyles.OptimizedDoubleBuffer | System.Windows.Forms.ControlStyles.UserPaint, true); */
 
             //источник света
             lamp = System.Numerics.Vector3.Normalize(new System.Numerics.Vector3(-1, 1, -1));
 
+			voxels = new System.Collections.Generic.List<Voxel>();
+
             //загружаем карту высот
-            using (var heightMap = (System.Drawing.Bitmap)System.Drawing.Bitmap.FromFile(BusEngine.Engine.DataDirectory + "Textures/heightmap.png"))
-            {
+            using (System.Drawing.Bitmap heightMap = (System.Drawing.Bitmap)System.Drawing.Bitmap.FromFile(BusEngine.Engine.DataDirectory + "Textures/heightmap.png")) {
                 //создаем обертку для быстрого доступа к пикселам
-                using (var wr = new System.Drawing.ImageWrapper(heightMap))
-                {
+                using (System.Drawing.ImageWrapper wr = new System.Drawing.ImageWrapper(heightMap)) {
                     //читаем карту высот, формируем воксели
-                    foreach (var p in wr)
-                    if(p.X > 0 && p.Y > 0)
-                    {
-                        //высота
-                        var height = wr[p].G;
-                        //высота в соседних точках
-                        var h1 = wr[p.X - 1, p.Y].G;
-                        var h2 = wr[p.X, p.Y - 1].G;
-                        //считаем градиент
-                        var dx = height - h1;
-                        var dy = height - h2;
-                        //считаем нормаль
-                        var n = new System.Numerics.Vector3(dx, NORMAL_Y, dy);
-                        n = System.Numerics.Vector3.Normalize(n);
-                        //считаем свет
-                        var light = (int)(System.Numerics.Vector3.Dot(n, lamp) * 255);
-                        if (light < 0) light = 0;
-                        if (light > 255) light = 255;
-                        //создаем воксель
-                        var voxel = new Voxel {Pos = new System.Numerics.Vector3(p.X, height * SCALE_HEIGHT, p.Y), Normal = n, Light = light};
-                        voxels.Add(voxel);
-                    }
+                    foreach (System.Drawing.Point p in wr) {
+						if (p.X > 0 && p.Y > 0) {
+							//высота
+							float height = wr[p].G;
+
+							//высота в соседних точках
+							float h1 = wr[p.X - 1, p.Y].G;
+							float h2 = wr[p.X, p.Y - 1].G;
+
+							//считаем градиент
+							float dx = height - h1;
+							float dy = height - h2;
+
+							//считаем нормаль
+							System.Numerics.Vector3 n = new System.Numerics.Vector3(dx, NORMAL_Y, dy);
+							n = System.Numerics.Vector3.Normalize(n);
+
+							//считаем свет
+							int light = (int)(System.Numerics.Vector3.Dot(n, lamp) * 255);
+
+							if (light < 0) {
+								light = 0;
+							}
+							if (light > 255) {
+								light = 255;
+							}
+
+							//создаем воксель
+							voxels.Add(new Voxel{
+								Pos = new System.Numerics.Vector3(p.X, height * SCALE_HEIGHT, p.Y),
+								Normal = n,
+								Light = light
+							});
+						}
+					}
                 }
 
                 //создаем результирующее изображение
@@ -121,36 +111,32 @@ namespace BusEngine.Game {
             }
 
             //задаем размер формы
-            BusEngine.UI.Canvas.WinForm.Size = new System.Drawing.Size(result.Width, 4 * result.Height / 5 + 60);
-            BusEngine.UI.Canvas.WinForm.BackColor = System.Drawing.Color.Black;
+            /* BusEngine.UI.Canvas.WinForm.Size = new System.Drawing.Size(result.Width, 4 * result.Height / 5 + 60);
+            BusEngine.UI.Canvas.WinForm.BackColor = System.Drawing.Color.Black; */
 
             //создаем трекбары
-            tbRoll = new System.Windows.Forms.TrackBar{ Parent = BusEngine.UI.Canvas.WinForm, Maximum = 180, Left = 10, Value = 0, Minimum = -180, Width = 200 };
-            tbPitch = new System.Windows.Forms.TrackBar{ Parent = BusEngine.UI.Canvas.WinForm, Maximum = 240, Left = 220, Value = 200, Minimum = 180, Width = 200};
+            tbRoll = new System.Windows.Forms.TrackBar{
+				Parent = BusEngine.UI.Canvas.WinForm,
+				Maximum = 180,
+				Minimum = -180,
+				Value = 0,
+				Left = 10,
+				Width = 200 
+			};
+            tbPitch = new System.Windows.Forms.TrackBar{
+				Parent = BusEngine.UI.Canvas.WinForm,
+				Maximum = 240,
+				Minimum = 180,
+				Value = 200,
+				Left = 220,
+				Width = 200
+			};
 
-            tbRoll.ValueChanged += tb_ValueChangedtbRoll;
-            tbPitch.ValueChanged += tb_ValueChangedtbPitch;
-			// Create a timer with a two second interval.
-			/* System.Timers.Timer aTimer = new System.Timers.Timer(1000/60);
-			// Hook up the Elapsed event for the timer. 
-			//aTimer.Elapsed += OnTimedEvent;
-			aTimer.AutoReset = true;
-			aTimer.Enabled = true; */
+            tbRoll.ValueChanged += new System.EventHandler(tb_ValueChangedtbRoll);
+            tbPitch.ValueChanged += new System.EventHandler(tb_ValueChangedtbPitch);
 
             tb_ValueChangedtbRoll(null, System.EventArgs.Empty);
 			tb_ValueChangedtbPitch(null, System.EventArgs.Empty);
-			BusEngine.Log.Info("The Elapsed event was raised at {0:HH:mm:ss.fff}", 3333);
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
 		}
 
 		// вызывается при отрисовки каждого кадра
@@ -180,6 +166,8 @@ namespace BusEngine.Game {
 		// перед закрытием BusEngine
 		public override void Shutdown() {
 			BusEngine.UI.Canvas.WinForm.Paint -= new System.Windows.Forms.PaintEventHandler(Paint1);
+            tbRoll.ValueChanged -= new System.EventHandler(tb_ValueChangedtbRoll);
+            tbPitch.ValueChanged -= new System.EventHandler(tb_ValueChangedtbPitch);
 		}
 
 		// событие мыши
@@ -192,7 +180,10 @@ namespace BusEngine.Game {
 		}
 
         private static void DoubleClick(object sender, System.EventArgs e) {
-            var sfd = new System.Windows.Forms.SaveFileDialog() {Title = "Сохранение 3D изображения", Filter = "Image|*.png"};
+            System.Windows.Forms.SaveFileDialog sfd = new System.Windows.Forms.SaveFileDialog() {
+				Title = "Сохранение 3D изображения",
+				Filter = "Image|*.png"
+			};
             if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
                 result.Save(sfd.FileName);
         }
@@ -209,73 +200,73 @@ namespace BusEngine.Game {
 
 
 
-        private static System.Drawing.Bitmap result;//результирующее 3d изображение
-        private static System.Collections.Generic.List<Voxel> voxels = new System.Collections.Generic.List<Voxel>();//список вокселей
+        private static System.Drawing.Bitmap result;
+        private static System.Collections.Generic.List<Voxel> voxels;
         private static System.Numerics.Vector3 lamp;//источник света
 
         private static System.Windows.Forms.TrackBar tbRoll;
         private static System.Windows.Forms.TrackBar tbPitch;
 
-        private static float pitch = 0;
-        private static float roll = 0;
+        private static float pitch = 0F;
+        private static float roll = 0F;
 
-        private const float SCALE_HEIGHT = 1 / 7f;
-        private const float NORMAL_Y = 10;
+        private const float SCALE_HEIGHT = 1F / 7F;
+        private const float NORMAL_Y = 10F;
 		
 		
 
-        void tb_ValueChangedtbRoll(object sender, System.EventArgs e) {
-			System.Threading.Tasks.Task.Run(() => {
-				roll = (float)(tbRoll.Value * System.Math.PI / 180);
-
+        private void tb_ValueChangedtbRoll(object sender, System.EventArgs e) {
+				double x = tbRoll.Value;
+				roll = (float)(x * System.Math.PI / 180D);
+			//System.Threading.Tasks.Task.Run(() => {
 				BusEngine.Engine.GameUpdate();
-			});
+			//});
         }
 
-        void tb_ValueChangedtbPitch(object sender, System.EventArgs e) {
-			System.Threading.Tasks.Task.Run(() => {
-				pitch = (float)(tbPitch.Value * System.Math.PI / 180);
-
+        private void tb_ValueChangedtbPitch(object sender, System.EventArgs e) {
+				double x = tbPitch.Value;
+				pitch = (float)(x * System.Math.PI / 180D);
+			//System.Threading.Tasks.Task.Run(() => {
 				BusEngine.Engine.GameUpdate();
-			});
+			//});
         }
-
-		private void OnTimedEvent(object source, System.Timers.ElapsedEventArgs e) {
-			//BusEngine.Log.Info("The Elapsed event was raised at {0:HH:mm:ss.fff}", e.SignalTime);
-            
-		}
-
 
 		private static void Paint1(object sender, System.Windows.Forms.PaintEventArgs e) {
 			System.Drawing.Graphics g = e.Graphics;
-			BusEngine.Log.Info("OnPaint");
+
             //матрицы вращения
-            var rotateM0 = System.Numerics.Matrix4x4.CreateRotationY(roll);
-            var rotateM = System.Numerics.Matrix4x4.CreateFromYawPitchRoll(0, pitch, 0);
+            System.Numerics.Matrix4x4 rotateM0 = System.Numerics.Matrix4x4.CreateRotationY(roll);
+            System.Numerics.Matrix4x4 rotateM = System.Numerics.Matrix4x4.CreateFromYawPitchRoll(0F, pitch, 0F);
 
             //матрица переноса
-            var position = new System.Numerics.Vector3(result.Width / 2f, 0, result.Height / 2f);
-            var translateM = System.Numerics.Matrix4x4.CreateTranslation(position);
-            var translateM0 = System.Numerics.Matrix4x4.CreateTranslation(-1 * position);
+            System.Numerics.Vector3 position = new System.Numerics.Vector3(result.Width / 2F, 0F, result.Height / 2F);
+            System.Numerics.Matrix4x4 translateM = System.Numerics.Matrix4x4.CreateTranslation(position);
+            System.Numerics.Matrix4x4 translateM0 = System.Numerics.Matrix4x4.CreateTranslation(-1F * position);
 
             //матрица смещения относительно экрана
-            var screenM = System.Numerics.Matrix4x4.CreateTranslation(new System.Numerics.Vector3(0, result.Height / 2f, 0));
+            System.Numerics.Matrix4x4 screenM = System.Numerics.Matrix4x4.CreateTranslation(new System.Numerics.Vector3(0, result.Height / 2f, 0F));
 
             //рендерим модель
-            using (var wr = new System.Drawing.ImageWrapper(result))
-            foreach (var v in voxels) {
-                //переводим в мировые координаты
-                var p = System.Numerics.Vector3.Transform(v.Pos, translateM0 * rotateM0 * rotateM * translateM * screenM);
-                var intX = (int) p.X;
-                var intY = (int) p.Y;
-                //цвет
-                var color = System.Drawing.Color.FromArgb(v.Light, v.Light, v.Light);
-                //заносим в изображение
-                wr[intX, intY + 1] = wr[intX, intY] = color;
-            }
+			using (new BusEngine.Benchmark("ImageWrapper")) {
+            using (System.Drawing.ImageWrapper wr = new System.Drawing.ImageWrapper(result)) {
+				System.Numerics.Matrix4x4 rotate = translateM0 * rotateM0 * rotateM * translateM * screenM;
+				foreach (Voxel v in voxels) {
+					//BusEngine.Log.Info("FPS Setting: {0}", v);
+					//переводим в мировые координаты
+					System.Numerics.Vector3 p = System.Numerics.Vector3.Transform(v.Pos, rotate);
+					int x = (int)p.X;
+					int y = (int)p.Y;
+					//цвет
+					System.Drawing.Color color = System.Drawing.Color.FromArgb(v.Light, v.Light, v.Light);
+					//заносим в изображение
+					wr[x, y] = wr[x, (y + 1)] = color;
+					
+				}
+			}
+			}
 
             //отрисовываем
-            g.DrawImage(result, new System.Drawing.PointF(0, 60));
+            g.DrawImage(result, new System.Drawing.Point(0, 60));
 		}
 	}
 
@@ -302,11 +293,11 @@ namespace System.Drawing {
         /// <summary>
         /// Ширина изображения
         /// </summary>
-        public int Width { get; private set; }
+        public static int Width { get; private set; }
         /// <summary>
         /// Высота изображения
         /// </summary>
-        public int Height { get; private set; }
+        public static int Height { get; private set; }
         /// <summary>
         /// Цвет по-умолачнию (используется при выходе координат за пределы изображения)
         /// </summary>
@@ -316,18 +307,18 @@ namespace System.Drawing {
         private byte[] outData;//выходной буфер
         private int stride;
         private System.Drawing.Imaging.BitmapData bmpData;
-        private Bitmap bmp;
+        private System.Drawing.Bitmap BMP;
 
         /// <summary>
         /// Создание обертки поверх bitmap.
         /// </summary>
         /// <param name="copySourceToOutput">Копирует исходное изображение в выходной буфер</param>
-        public ImageWrapper(Bitmap bmp, bool copySourceToOutput = false) {
+        public ImageWrapper(System.Drawing.Bitmap bmp, bool copySourceToOutput = false) {
             Width = bmp.Width;
             Height = bmp.Height;
-            this.bmp = bmp;
+            BMP = bmp;
 
-            bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
+            bmpData = BMP.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
             stride = bmpData.Stride;
 
             data = new byte[stride * Height];
@@ -342,29 +333,31 @@ namespace System.Drawing {
         /// </summary>
         public Color this[int x, int y] {
             get {
-                var i = GetIndex(x, y);
-                return i < 0 ? DefaultColor : Color.FromArgb(data[i + 3], data[i + 2], data[i + 1], data[i]);
+                int i = GetIndex(x, y);
+
+                return (i < 0 ? DefaultColor : System.Drawing.Color.FromArgb(data[(i + 3)], data[(i + 2)], data[(i + 1)], data[i]));
             } set {
-                var i = GetIndex(x, y);
-                if(i >= 0) {
+                int i = GetIndex(x, y);
+
+                if (i >= 0) {
                     outData[i] = value.B;
-                    outData[i + 1] = value.G;
-                    outData[i + 2] = value.R;
-                    outData[i + 3] = value.A;
+                    outData[(i + 1)] = value.G;
+                    outData[(i + 2)] = value.R;
+                    outData[(i + 3)] = value.A;
                 };
             }
         }
 
         public Color GetOutputPixel(int x, int y) {
-            var i = GetIndex(x, y);
-            return i < 0 ? DefaultColor : Color.FromArgb(outData[i + 3], outData[i + 2], outData[i + 1], outData[i]);
+            int i = GetIndex(x, y);
+            return i < 0 ? DefaultColor : System.Drawing.Color.FromArgb(outData[i + 3], outData[i + 2], outData[i + 1], outData[i]);
         }
 
         /// <summary>
         /// Возвращает пиксел из исходнго изображения.
         /// Либо заносит пиксел в выходной буфер.
         /// </summary>
-        public Color this[Point p] {
+        public Color this[System.Drawing.Point p] {
             get { return this[p.X, p.Y]; }
             set { this[p.X, p.Y] = value; }
         }
@@ -373,29 +366,41 @@ namespace System.Drawing {
         /// Заносит в выходной буфер значение цвета, заданные в double.
         /// Допускает выход double за пределы 0-255.
         /// </summary>
-        public void SetPixel(Point p, double r, double g, double b, double a = 255) {
-            if (r < 0) r = 0;
-            if (r >= 256) r = 255;
-            if (g < 0) g = 0;
-            if (g >= 256) g = 255;
-            if (b < 0) b = 0;
-            if (b >= 256) b = 255;
-            if (a < 0) a = 0;
-            if (a >= 256) a = 255;
+        public void SetPixel(System.Drawing.Point p, double r, double g, double b, double a = 255) {
+            if (r < 0) {
+				r = 0;
+			} else if (r > 255) {
+				r = 255;
+			}
+            if (g < 0) {
+				g = 0;
+			} else if (g > 255) {
+				g = 255;
+			}
+            if (b < 0) {
+				b = 0;
+			} else if (b > 255) {
+				b = 255;
+			}
+            if (a < 0) {
+				a = 0;
+			} else if (a > 255) {
+				a = 255;
+			}
 
-            this[p.X, p.Y] = Color.FromArgb((int)a, (int)r, (int)g, (int)b);
+            this[p.X, p.Y] = System.Drawing.Color.FromArgb((int)a, (int)r, (int)g, (int)b);
         }
 
-        public void SetPixelUnsafe(int x, int y, int r, int g, int b, int a = 255) {
+        /* public void SetPixelUnsafe(int x, int y, int r, int g, int b, int a = 255) {
             var i = (x << 2) + y * stride;
             outData[i] = (byte)b;
             outData[i + 1] = (byte)g;
             outData[i + 2] = (byte)r;
             outData[i + 3] = (byte)a;
-        }
+        } */
 
-        int GetIndex(int x, int y) {
-            return (x < 0 || x >= Width || y < 0 || y >= Height) ? -1 : x * 4 + y * stride;
+        public int GetIndex(int x, int y) {
+            return (x < 0 || x >= Width || y < 0 || y >= Height ? -1 : x * 4 + y * stride);
         }
 
         /// <summary>
@@ -404,16 +409,20 @@ namespace System.Drawing {
         /// </summary>
         public void Dispose() {
             System.Runtime.InteropServices.Marshal.Copy(outData, 0, bmpData.Scan0, outData.Length);
-            bmp.UnlockBits(bmpData);
+            BMP.UnlockBits(bmpData);
         }
 
         /// <summary>
         /// Перечисление всех точек изображения
         /// </summary>
         public System.Collections.Generic.IEnumerator<System.Drawing.Point> GetEnumerator() {
-            for (int y = 0; y < Height; y++)
-                for (int x = 0; x < Width; x++)
+			int x, y;
+
+            for (y = 0; y < Height; ++y) {
+                for (x = 0; x < Width; ++x) {
                     yield return new System.Drawing.Point(x, y);
+				}
+			}
         }
 
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
