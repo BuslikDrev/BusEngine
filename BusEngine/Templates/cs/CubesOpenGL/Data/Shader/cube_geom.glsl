3e63a1a8ae90ca0ced034686b16ae21098b7c521
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2023-12-27 06:54:27 +0300

    Add files via upload

--- BusEngine/Templates/cs/CubesOpenGL/Data/Shader/cube_geom.glsl
+++ BusEngine/Templates/cs/CubesOpenGL/Data/Shader/cube_geom.glsl
@@ -1,9 +1,10 @@
 #version 330 core
+#extension ARB_geometry_shader4 : enable
 
 //https://www.khronos.org/opengl/wiki/Built-in_Variable_(GLSL)
 //https://www.khronos.org/opengl/wiki/Geometry_Shader
 layout (TRIANGLES) in;
-layout (TRIANGLE_STRIP, max_vertices = 48) out;
+layout (TRIANGLE_STRIP, max_vertices = 128) out;
 
 in vec4 gColors[];
 out vec4 fColors; 
@@ -14,20 +15,26 @@ int i, x = 0, y = 0;
 vec4 pos = vec4(0.0F, 0.0F, 0.0F, 0.0F);
 
 void main() {
+	vec4 t = gl_in[0].gl_Position;
+	vec4 h = gl_in[1].gl_Position;
+	vec4 n = gl_in[2].gl_Position;
+
+	fColors = gColors[2];
+
 	for (i = 0; i < 16; i++) {
 		pos.x = 3.0F * x;
 		pos.y = 3.0F * y;
 
-		gl_Position = gl_in[0].gl_Position + pos * VP;
-		fColors = gColors[0];
+		gl_Position = pos * VP + t;
+		//fColors = gColors[0];
 		EmitVertex();
 
-		gl_Position = gl_in[1].gl_Position + pos * VP;
-		fColors = gColors[1];
+		gl_Position = pos * VP + h;
+		//fColors = gColors[1];
 		EmitVertex();
 
-		gl_Position = gl_in[2].gl_Position + pos * VP;
-		fColors = gColors[2];
+		gl_Position = pos * VP + n;
+		//fColors = gColors[2];
 		EmitVertex();
 
 		EndPrimitive();
--- BusEngine/Templates/cs/CubesOpenGL/Data/Shader/cube_vert.glsl
+++ BusEngine/Templates/cs/CubesOpenGL/Data/Shader/cube_vert.glsl
@@ -8,8 +8,10 @@ out vec4 gColors;
 uniform mat4 A;
 uniform mat4 P;
 uniform mat4 VP;
+//out float gl_ClipDistance[1];
 
 void main() {
 	gl_Position = p * (A * P * VP);
 	gColors = c;
+	//gl_ClipDistance[0] = p;
 }
